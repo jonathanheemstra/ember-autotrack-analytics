34b7a91bddcd3f9e2740cb892c37659bedf35f76
jonathanheemstra <jonathan.heemstra@gmail.com>
2018-11-01 09:25:02 -0700

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-autotrack-analytics'
+  name: 'ember-autotrack-analytics',
+
+  contentFor(type, config) {
+    let content;
+
+    if (type === 'head') {
+      // const analyticsConfig = config['analytics'] || {};
+      // const params = [];
+
+      // if (analyticsConfig.key) {
+      //   params.push('')
+      // }
+
+      content = `
+        <script>
+        window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+          ga('create', 'UA-5025626-34', 'auto');
+
+          ga('require', 'eventTracker');
+          ga('require', 'outboundLinkTracker');
+          ga('require', 'urlChangeTracker');
+
+          ga('send', 'pageview');
+        </script>
+        <script async src="//www.google-analytics.com/analytics.js"></script>
+      `;
+    }
+
+    return content;
+  }
 };
